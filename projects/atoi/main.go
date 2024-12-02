a1190b94ac2da02ea2380c0fa9009da889496d80
tim0shkaa <talex05@inbox.ru>
2024-12-03 02:35:37 +0300

    laba3

--- projects/atoi/main.go
+++ projects/atoi/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var input string
+	fmt.Print("Введите целое число: ")
+	fmt.Scan(&input)
+
+	result := ""
+
+	for _, char := range input {
+		digit, err := strconv.Atoi(string(char))
+		if err != nil {
+			fmt.Println("Ошибка при преобразовании символа в цифру:", err)
+			return
+		}
+		square := digit * digit
+		result += strconv.Itoa(square)
+	}
+
+	fmt.Println("Результат:", result)
 }
--- projects/triangle/main.go
+++ projects/triangle/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var a, b float64
+	fmt.Println("Введите два положительных числа через пробел:")
+
+	_, err := fmt.Scan(&a, &b)
+	if err != nil {
+		fmt.Println("Ошибка ввода:", err)
+		return
+	}
+
+	if a <= 0 || b <= 0 {
+		fmt.Println("Оба числа должны быть положительными")
+		return
+	}
+
+	fmt.Println(math.Sqrt(a*a + b*b))
 }
